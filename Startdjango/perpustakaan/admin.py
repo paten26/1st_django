8ea34297ec3b27b702804a8480e70d429c9c2672
LUKAS YULIANTO <paten26@gmail.com>
2024-09-23 22:32:35 +0700

    admin login

--- Startdjango/perpustakaan/admin.py
+++ Startdjango/perpustakaan/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Perpustakaan
+
+admin.site.register(Perpustakaan)
 # Register your models here.
--- Startdjango/perpustakaan/models.py
+++ Startdjango/perpustakaan/models.py
@@ -3,4 +3,6 @@ from django.db import models
 class Perpustakaan(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    penulis = models.CharField(max_length=200, default="NA")
+    stok = models.IntegerField(default=1)
 # Create your models here.
